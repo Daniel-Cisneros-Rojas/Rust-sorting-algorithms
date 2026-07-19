a09f58f5373a020e49193056c5053de98ae53afb
Daniel-Cisneros-Rojas <dcr.astrodactil@gmail.com>
2026-07-18 22:09:44 -0600

    algoritmo ordenamiento seleccion

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use rand::Rng;
 fn main() {
     println!("Algoritmos de ordenamiento");
-    let mut numeros = numeros_aleatorios(20);
+    let mut numeros = numeros_aleatorios(10);
+    let mut numeros2=numeros.clone();
     println!("Números generados aleatoriamente : {:?}", numeros);
     //let mut num=vec![5, 3, 8, 4, 2];
-    bubble_sort(&mut numeros);
+    //num.swap(0, 0);
+    bubble_sort( &mut numeros);
+    selection_sort(&mut numeros2);
     println!("{:?}", numeros);
 
 }
@@ -20,6 +23,7 @@ fn numeros_aleatorios(tam:i32)-> Vec<i32> {
 }
 
 fn bubble_sort<T: Ord + std::fmt::Debug>(datos: &mut[T]){
+    println!("Algoritmo ordenamiento burbuja\n");
    let tam= datos.len();
     for i in 0..tam-1{
         for j in 0..tam-1-i{
@@ -32,3 +36,18 @@ fn bubble_sort<T: Ord + std::fmt::Debug>(datos: &mut[T]){
     }
 }
 
+
+fn selection_sort<T: Ord + std::fmt::Debug>(datos: &mut[T]){
+    println!("Algoritmo ordenamiento selección\n");
+    let tam= datos.len();
+    for i in 0..tam-1{
+        let mut index_menor_numero=i;
+        for j in i+1..tam{
+            if datos[index_menor_numero]>datos[j]{
+                index_menor_numero=j;
+            }
+        }
+        datos.swap(i, index_menor_numero);
+        println!("{:?} vuelta {}", datos, i);
+    }
+}
